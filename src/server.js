4ab35c3c12941012046f03f8c1486f0801e071b8
Robert Shepley <shepleysound@gmail.com>
2022-09-22 23:45:58 -0700

    Refactor routes with DRY principles.

--- src/server.js
+++ src/server.js
@@ -9,15 +9,17 @@ const notFound = require('./middleware/error-handlers/404');
 const errorHandler = require('./middleware/error-handlers/500');
 
 // Import routes
-const usersRouter = require('./routes/users');
-const booksRouter = require('./routes/books');
+// const usersRouter = require('./routes/users');
+// const booksRouter = require('./routes/books');
+const apiRouter = require('./routes/v1.js');
 
 const app = express();
 
 app.use(logger);
 app.use(express.json());
-app.use(usersRouter);
-app.use(booksRouter);
+// app.use(usersRouter);
+// app.use(booksRouter);
+app.use('/api/v1/:route', apiRouter);
 
 app.get('/', (req, res, next) => {
   res.status(200).send('Welcome to the server!');
